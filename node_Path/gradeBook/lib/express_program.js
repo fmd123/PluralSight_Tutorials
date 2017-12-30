93ad84252f4db258bfb19f306cf75148f4ca29fc
fmd123 <fmdavis6@gmail.com>
2017-12-30 15:34:19 -0700

    simple server

---
+++ node_Path/gradeBook/lib/express_program.js
@@ -0,0 +1,29 @@
+var book = require('./grades.js').book;
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.send("Hello World :-)")
+});
+
+app.get('/grade', function(req,res){
+  var average = 0;
+  var letterGrade = '';
+  var grades = req.query.grades.split(",");
+  console.log(grades);
+  for(var i=0; i<grades.length; i++){
+    book.addGrade(parseInt(grades[i]))
+  }
+
+  average = book.getAverage();
+  letterGrade = book.getLetterGrade();
+
+  console.log("below is req.query")
+  console.log(req.query);
+
+  res.send("Your average is: " + average + " and your grade is: " + letterGrade)
+
+
+})
+app.listen(3000);
+console.log("server is ready... ")
--- node_Path/gradeBook/lib/grades.js
+++ node_Path/gradeBook/lib/grades.js
@@ -1,9 +1,10 @@
 var gradeBook = {
 
   _grades: [],
-
+  result: 0,
   reset: function(){
     this._grades =[];
+
   },
   addGrade: function(newGrade){
     this._grades.push(newGrade);
@@ -15,11 +16,39 @@ var gradeBook = {
 
   getAverage: function(){
     var total = 0;
+    var result = 0;
     for(i=0; i<this._grades.length; i++){
       total += this._grades[i]
     }
-    return total/this._grades.length;
+    result =  total/this._grades.length;
+    console.log(this._grades);
+    console.log('this is result: ' + result);
+    console.log(total)
+    return result;
+  },
 
+  getLetterGrade: function () {
+    var resulting = 0;
+    resulting = this.getAverage();
+    console.log(resulting)
+    if(resulting >=90){
+      this.reset();
+      return "A"
+    }else if (resulting >=80){
+      this.reset();
+      return 'B'
+
+    }else if (resulting >=70){
+      this.reset();
+      return 'C'
+    }else if (resulting >=60){
+      this.reset();
+      return 'D'
+    }else{
+      this.reset();
+      return 'F'
+
+    }
   }
 
 
